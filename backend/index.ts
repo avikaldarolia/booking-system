cf27e51e4cb9d9663e9e237299fd49fba5a40b00
Avikal Darolia <avikaldarolia7@gmail.com>
2025-03-02 23:29:47 -0500

    base setup

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+
+const PORT = 8000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.disable("x-powered-by");
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
