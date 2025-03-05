2864c9b2b2c11dcbd45c3efeadf7e14ae0e6645f
Avikal Darolia <avikaldarolia7@gmail.com>
2025-03-05 15:46:42 -0500

    Google Calendar integrated.

--- backend/third-party/google-calendar/googleCalender.ts
+++ backend/third-party/google-calendar/googleCalender.ts
@@ -5,7 +5,7 @@ import { Shift } from "../../entities/Shift";
 // Initialize Google Calendar API
 const auth = new google.auth.JWT({
 	email: process.env.GOOGLE_CLIENT_EMAIL,
-	key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+	key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
 	scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
